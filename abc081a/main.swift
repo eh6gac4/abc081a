8bd679b4745a71d444bb491e7f8b3ca8b9e85e06
Toshiki Cho <toshiki.cho.dev@outlook.com>
2023-02-24 19:56:49 +0900

    initial commit

--- abc081a/main.swift
+++ abc081a/main.swift
@@ -2,10 +2,17 @@
 //  main.swift
 //  abc081a
 //
-//  Created by Cho Toshiki on 2023/02/24.
-//
 
 import Foundation
 
-print("Hello, World!")
+// 入力された文字列に対して、1の数をカウントする
+// map()で文字列->整数型の変換を容易に
+let input = readLine() ?? "000" //入力例: 101
+let inputsInt = input.map({Int(String($0))!})
 
+var sum = 0
+for i in inputsInt {
+    sum = sum + i
+}
+
+print(sum)
